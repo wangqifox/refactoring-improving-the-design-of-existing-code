e0b331782883c5803f97e437b5584483b821760b
wangqi <wangq2880@163.com>
2019-05-05 16:08:16 +0800

    chapter01

--- src/main/java/chapter01/ver01/NewReleasePrice.java
+++ src/main/java/chapter01/ver01/NewReleasePrice.java
@@ -15,4 +15,9 @@ public class NewReleasePrice extends Price {
     public double getCharge(int daysRented) {
         return daysRented * 3;
     }
+
+    @Override
+    int getFrequentRenterPoints(int daysRented) {
+        return daysRented > 1 ? 2 : 1;
+    }
 }
--- src/main/java/chapter01/ver01/Price.java
+++ src/main/java/chapter01/ver01/Price.java
@@ -9,4 +9,8 @@ public abstract class Price {
     abstract int getPriceCode();
 
     abstract double getCharge(int daysRented);
+
+    int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
